8816a6258751b9966d4640efd140919821e1f3e0
SandipAigole <sandipaigole@gmail.com>
2018-10-26 14:15:42 +0530

    Commited

---
+++ src/MapDemo.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Player{
+	int jn;
+	String name;
+	String team;
+	int age;
+	
+	Player(int jn,String name,String team,int age)
+	{
+		this.jn=jn;
+		this.name=name;
+		this.team=team;
+		this.age=age;
+	}
+}
+
+public class MapDemo {
+	
+	public static void main(String []args)
+	{
+		Player p1=new Player(7, "Cristano", "portugal", 24);
+		Player p2=new Player(10, "Neymar", "Brazil", 28);
+		Player p3=new Player(17, "Virat", "India", 25);
+		
+		Map<Integer,Player> m=new HashMap<Integer,Player>(); 
+		m.put(1,p1);
+		m.put(2,p2);
+		m.put(3,p3);
+		
+		for (Integer k : m.keySet()) {
+			
+			System.out.println("key="+k);
+			Player p=m.get(k);
+			System.out.println("player="+p.name);
+		}
+		
+	}
+}	
+	
---
+++ src/MapDemoTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MapDemoTest {
+
+	@Test
+	void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	
+	@Test
+	void testEquals() {
+		fail("Not yet implemented");
+	}
+
+}
